732337996e79023a24b62c570fc2ee4ed0b85f37
Botir <botirqulturayev01@gmail.com>
2023-06-21 20:11:56 +0500

    I finished the html structure of the scroll page

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".menyu_slide", {
+    slidesPerView: 4.2,
+    spaceBetween: 16,
+    // centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      
+    },
+});
