5ec729ccaabbd3148614a96a454fa49bf8304b7c
Jurabek <jurabektukhtayev98@gmail.com>
2023-08-02 12:10:25 +0500

    yana

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const productRemove = document.getElementById('product_remove');
-const productAdd = document.getElementById('product_add');
-const prices = document.getElementById('total_price');
-
-prices.addEventListener('click', function (e) {
-
-})
