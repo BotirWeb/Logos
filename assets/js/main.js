60d074b0b605d9664d0191ef09b70869abd44729
Jurabek <jurabektukhtayev98@gmail.com>
2023-08-02 12:00:13 +0500

    main page done

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const productRemove = document.getElementById('product_remove');
+const productAdd = document.getElementById('product_add');
+const prices = document.getElementById('total_price');
+
+prices.addEventListener('click', function (e) {
+
+})
